372eeed8fb397cfe3f83165c73ed1d14d2a5704f
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 09:35:56 +0500

    Add files via upload

---
+++ qno22.js
@@ -0,0 +1,6 @@
+// QNO:22
+// Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+// let error =["iqra ", " qirat" , " ahmed" ,"irfan" , "sajjad"];
+// console.log(error[9])
+// let nonerror =["iqra ", " qirat" , " ahmed" ,"irfan" , "sajjad"];
+// console.log(nonerror[4])
